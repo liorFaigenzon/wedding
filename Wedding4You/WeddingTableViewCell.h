323bb8b03c05a3e3c25c3e086cfdfc83a5bf1105
Admin <admin@macintosh.local>
2016-01-15 14:23:45 +0200

    xxx

---
+++ Wedding4You/WeddingTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  WeddingTableViewCell.h
+//  Wedding4You
+//
+//  Created by Admin on 1/14/16.
+//  Copyright © 2016 Faigenzon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WeddingTableViewCell : UITableViewCell
+
+@end
